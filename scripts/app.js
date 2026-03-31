67530a80d73e4b8dbecd8fb0dacae59898cac4e2
jahanjaved <jahanjaved1981@gmail.com>
2026-03-31 17:25:20 +0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,25 @@
-
 fetch('data/site-data.json')
-.then(res => res.json())
-.then(data => {
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById('summary').innerHTML =
+      `<p>Total Inspections: ${data.inspections.length}</p>
+       <p>Total Actions: ${data.actions.length}</p>`;
 
-  document.getElementById('summary').innerHTML =
-    `<p>Total Inspections: ${data.inspections.length}</p>
-     <p>Total Actions: ${data.actions.length}</p>`;
+    let insp = '<table><tr><th>ID</th><th>Cluster</th></tr>';
+    data.inspections.forEach(i => {
+      insp += `<tr><td>${i.id}</td><td>${i.cluster}</td></tr>`;
+    });
+    insp += '</table>';
+    document.getElementById('inspections').innerHTML = insp;
 
-  let insp = '<table><tr><th>ID</th><th>Cluster</th></tr>';
-  data.inspections.forEach(i=>{
-    insp += `<tr><td>${i.id}</td><td>${i.cluster}</td></tr>`;
+    let act = '<table><tr><th>ID</th><th>Status</th></tr>';
+    data.actions.forEach(a => {
+      act += `<tr><td>${a.id}</td><td>${a.status}</td></tr>`;
+    });
+    act += '</table>';
+    document.getElementById('actions').innerHTML = act;
+  })
+  .catch(err => {
+    document.getElementById('summary').innerHTML = '<p>Data failed to load.</p>';
+    console.error(err);
   });
-  insp += '</table>';
-  document.getElementById('inspections').innerHTML = insp;
-
-  let act = '<table><tr><th>ID</th><th>Status</th></tr>';
-  data.actions.forEach(a=>{
-    act += `<tr><td>${a.id}</td><td>${a.status}</td></tr>`;
-  });
-  act += '</table>';
-  document.getElementById('actions').innerHTML = act;
-
-});
